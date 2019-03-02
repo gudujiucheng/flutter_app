6a23799aa281e667bac3cfd847a0038327347e81
zhangke <zhangke01@fangdd.com>
2019-03-02 15:53:01 +0800

    第一个flutter程序，写一个hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());//dart 中单行函数的写法
 
-class MyApp extends StatelessWidget {//不可变的控件
-
+class MyApp extends StatelessWidget {//继承了一个weight  StatelessWidget
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      home: new RandomWords(),
-    );
-  }
-//  @override
-//  Widget build(BuildContext context) {
-//
-//    return new MaterialApp(
-//      title: 'Welcome to Flutter',
-//      home: new Scaffold(
-//        appBar: new AppBar(
-//          title: new Text('Welcome to Flutter'),
-//        ),
-//        body: new Center(
-//          //child: new Text(wordPair.asPascalCase),
-//          child: new RandomWords(),
-//        ),
-//      ),
-//    );
-////    final word = new WordPair.random();
-////    return MaterialApp(
-////      title: 'Welcome to Flutter',
-////      home: Scaffold(
-////        appBar: AppBar(
-////          title: Text('Welcome to xxxx Flutter'),
-////        ),
-////        body: Center(
-//////          child: Text('Hello 66666 World'),
-////          child: new Text(word.asPascalCase),
-////        ),
-////      ),
-////    );
-//  }
-}
-
-class RandomWords extends StatefulWidget {//有状态的
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];//在Dart语言中使用下划线前缀标识符，会强制其变成私有的。
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
-//    final wordPair = new WordPair.random();
-//    return new Text(wordPair.asPascalCase);
-    return new Scaffold (
-      appBar: new AppBar(
-        title: new Text('xxxxx Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(20.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该行书湖添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      title: 'Welcome to Flutter',
+      home: new Scaffold(//一个weight 提供默认导航栏和标题等
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text('Hello World'),
+        ),
       ),
     );
   }
-
-
 }
