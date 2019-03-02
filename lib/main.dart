4bfc8a8b5460d5514a69e300e5d738e0bf2bd32e
zhangke <zhangke01@fangdd.com>
2019-03-02 16:16:05 +0800

    添加一个有状态的weight

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,32 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),//调用驼峰格式
+//          child: new Text(wordPair.asPascalCase),//调用驼峰格式
+          child: new RandomWords(),//调用驼峰格式
         ),
       ),
     );
+  }
+}
+
+
+
+// StatelessWidget 是不可变的，意味着属性不能改变，所有的值都是最终的
+// StatefulWidget 在生命周期内是可以变的，实现一个StatefulWidget 至少需要两个类
+//1、StatelessWidget
+//2、State类 StatelessWidget本身是不变的，但是 state在weight声明周期始终存在
+
+class RandomWords extends StatefulWidget{
+  @override
+  createState()=>new RandomWordsState();
+
+}
+
+//该类持有 RandomWords weight 的状态
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    var wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
