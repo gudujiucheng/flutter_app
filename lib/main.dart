463474e14effa3477bfde22eae4218f38b5e10d0
zhangke <zhangke01@fangdd.com>
2019-03-02 16:00:44 +0800

    测试引入外部包

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());//dart 中单行函数的写法
+//测试引入外部包
+import 'package:english_words/english_words.dart';
 
-class MyApp extends StatelessWidget {//继承了一个weight  StatelessWidget
+void main() => runApp(new MyApp()); //dart 中单行函数的写法
+
+class MyApp extends StatelessWidget {
+  //继承了一个weight  StatelessWidget
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new Scaffold(//一个weight 提供默认导航栏和标题等
+      home: new Scaffold(
+        //一个weight 提供默认导航栏和标题等
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),//调用驼峰格式
         ),
       ),
     );
